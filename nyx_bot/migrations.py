407c6540037c9ce7279cad3e37e5a33cbd89fadf
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2022-11-01 17:51:13 +0800

    Update

--- nyx_bot/migrations.py
+++ nyx_bot/migrations.py
@@ -14,10 +14,9 @@ def migrate_db(db):
     version_item = DatabaseVersion.get_or_none()
     if version_item is None:
         version_item = DatabaseVersion()
-        version_item.version = 0
-    match version_item.version:
-        case 1:
-            migrate(migrator.add_column("usertag", "locked", UserTag.locked))
+        version_item.version = 1
+    if version_item.version == 1:
+        migrate(migrator.add_column("usertag", "locked", UserTag.locked))
 
     version_item.version = 2
     version_item.save()
